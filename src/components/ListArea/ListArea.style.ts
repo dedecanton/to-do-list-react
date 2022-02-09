61e7efb2cdd4d06bcaebcc3ccf3fcf31d2764902
dedecanton <andrecanton18@gmail.com>
2022-02-09 10:56:39 -0300

    margin ListArea

--- src/components/ListArea/ListArea.style.ts
+++ src/components/ListArea/ListArea.style.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   box-sizing: border-box;
-  margin: 0 auto;
+  margin: 1rem auto;
   max-width: 900px;
   width: 95%;
   display: flex;
@@ -10,7 +10,6 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
   box-shadow: 1px 1px 6px rgb(0 0 0 / 80%);
-  // border: 2px solid black;
   border-radius: 0.5rem;
   padding: 1rem;
 
